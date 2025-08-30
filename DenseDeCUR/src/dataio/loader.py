eca045605249db2d51afd59c61400a7dc4570d66
Daniele Kopyshevskiy <81174786+Kopyshevskiy@users.noreply.github.com>
2025-08-30 18:27:37 +0200

    update loader.py

--- DenseDeCUR/src/dataio/loader.py
+++ DenseDeCUR/src/dataio/loader.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import numpy as np
 from torchvision import transforms
+from cvtorchvision import cvtransforms
+from .rs_transforms_uint8 import RandomChannelDrop, RandomBrightness, RandomContrast, ToGray, GaussianBlur, Solarize
+
 
 class TwoCropsTransform:
     """prende un'immagine e restituisce due versioni augmentate q e k, in base a base_transform"""
@@ -11,28 +14,24 @@ class TwoCropsTransform:
         k = self.base_transform(x)
         return [q, k]
 
-class NumpyToPIL:
-    def __call__(self, x):
-        return Image.fromarray(x) if isinstance(x, np.ndarray) else x
 
 def kaist_rgb_train_transforms(img_size=224):
-    return transforms.Compose([
-        NumpyToPIL(),
-        transforms.RandomResizedCrop(img_size, scale=(0.5, 1.0)),
-        transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(0.4, 0.4, 0.4, 0.1),
-        transforms.RandomGrayscale(p=0.2),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]),
+    return cvtransforms.Compose([
+        cvtransforms.RandomResizedCrop(img_size, scale=(0.5, 1.0)),
+        cvtransforms.RandomApply([RandomBrightness(0.4), RandomContrast(0.4)], p=0.8),
+        cvtransforms.RandomApply([ToGray(3)], p=0.2),
+        cvtransforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5),
+        cvtransforms.RandomHorizontalFlip(),
+        cvtransforms.ToTensor(),
+        cvtransforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]),
     ])
 
 def kaist_th_train_transforms(img_size=224):
-    return transforms.Compose([
-        NumpyToPIL(),
-        transforms.RandomResizedCrop(img_size, scale=(0.5, 1.0)),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]),
+    return cvtransforms.Compose([
+        cvtransforms.RandomResizedCrop(img_size, scale=(0.5, 1.0)),
+        cvtransforms.RandomHorizontalFlip(),
+        cvtransforms.ToTensor(),
+        cvtransforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]),
     ])
 
 def build_kaist_transforms(img_size=224):
